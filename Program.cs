c789d94b7be36a79eb29fd3125d87dde41341a2b
Gessielle <gessielle-barossi@hotmail.com>
2020-10-10 18:24:34 -0300

    numeral extenso

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Humanizer;
+
+namespace Numeral_Extenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string texto;
+
+            Console.Write("Digite um Número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            texto = numero.ToWords();
+            Console.Write("Seu numero em texto é: ");
+            Console.WriteLine (texto);
+        }
+    }
+}
